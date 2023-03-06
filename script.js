a467e45ee984ea387fb77f6138e03464729e0991
jclem28 <jclem2806@gmail.com>
2023-03-05 22:21:26 -0600

    initial commit, with some html and css

---
+++ script.js
@@ -0,0 +1,6 @@
+getValues = () => {
+  const loanAmount = document.getElementById("amount");
+  const downPayment = document.getElementById("downpayment");
+  const interestRate = document.getElementById("interest-rate");
+  const loanTerm = document.getElementById("term");
+};
