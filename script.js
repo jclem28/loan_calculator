c48104ae0be365a48cb4375ae7895044a1976eb6
jclem28 <jclem2806@gmail.com>
2023-03-06 21:56:47 -0600

    added calc functionality

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-getValues = () => {
-  const loanAmount = document.getElementById("amount");
-  const downPayment = document.getElementById("downpayment");
-  const interestRate = document.getElementById("interest-rate");
-  const loanTerm = document.getElementById("term");
+let loanAmount = document.getElementById("amount").value;
+let downPayment = document.getElementById("downpayment").value;
+let interestRate = document.getElementById("interest-rate").value;
+let loanTerm = document.getElementById("term").value;
+
+let monthlyPaymentValue = document.getElementById("monthly-payment-value");
+let totalPrincipleValue = document.getElementById("total-principle-value");
+let totalInterestValue = document.getElementById("total-interest-value");
+let totalCostValue = document.getElementById("total-cost-value");
+
+const compute = document.querySelector("button");
+
+loanAmount = parseFloat(loanAmount);
+downPayment = parseFloat(downPayment);
+interestRate = parseFloat(interestRate);
+loanTerm = parseFloat(loanTerm);
+
+let interest = interestRate / 12 / 100;
+let newLoanAmount = loanAmount - downPayment;
+
+monthlyPayment = () => {
+  let payment =
+    (newLoanAmount * interest * Math.pow(1 + interest, loanTerm)) /
+    (Math.pow(1 + interest, loanTerm) - 1);
+
+  return payment;
 };
+
+updateData = (payment) => {
+  monthlyPaymentValue.innerText = Math.round(payment);
+
+  let totalPrinciple = Math.round(loanTerm * payment);
+  totalPrincipleValue.innerHTML = totalPrinciple;
+
+  let totalInterestPayable = Math.round(totalPrinciple - newLoanAmount);
+  totalInterestValue.innerHTML = totalInterestPayable;
+
+  let totalCost = Math.round(totalPrinciple + totalInterestPayable);
+  totalCostValue.innerText = totalCost;
+};
+
+init = () => {
+  let myPayment = monthlyPayment();
+  updateData(myPayment);
+};
+
+// Event Listener for Compute Button
+compute.addEventListener("click", init);
